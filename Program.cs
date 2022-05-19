f51428b036be0296d9f94d8b4b710a1e7ec4694e
Renan Malagutti <renanmalagutti.dev@gmail.com>
2022-05-19 16:40:41 -0300

    fix date sql cmd

--- Program.cs
+++ Program.cs
@@ -76,7 +76,7 @@ END;";
                 sw.WriteLine(createTableSQL);
                 foreach (var registro in objetoRegistroFromJson.Registros)
                 {
-                    string lineInsertSQL = $@"INSERT INTO {sqlTableName}(Estabelecimento,CNPJ,DataMovimento,ValorContabil,QuantidadeCuponsCancelados,NumeroEquipamentoSAT,NumeroCFeInicial,NumeroCFeFinal,NumeroLinha) VALUES ('{objetoRegistroFromJson.Estabelecimento}','{objetoRegistroFromJson.CNPJ}',{registro.Data.ToString("yyyy-MM-dd")},{registro.ValorContabil.ToString().Replace(",", ".")},{registro.QuantidadeCuponsCancelados},{registro.NumeroEquipamentoSAT},{registro.NumeroCFeInicial.ToString()},{registro.NumeroCFeFinal.ToString()},{registro.NumeroLinha});";
+                    string lineInsertSQL = $@"INSERT INTO {sqlTableName}(Estabelecimento,CNPJ,DataMovimento,ValorContabil,QuantidadeCuponsCancelados,NumeroEquipamentoSAT,NumeroCFeInicial,NumeroCFeFinal,NumeroLinha) VALUES ('{objetoRegistroFromJson.Estabelecimento}','{objetoRegistroFromJson.CNPJ}','{registro.Data.ToString("yyyy-MM-dd")}',{registro.ValorContabil.ToString().Replace(",", ".")},{registro.QuantidadeCuponsCancelados},{registro.NumeroEquipamentoSAT},{registro.NumeroCFeInicial.ToString()},{registro.NumeroCFeFinal.ToString()},{registro.NumeroLinha});";
                     sw.WriteLine(lineInsertSQL);
                 }
             }
